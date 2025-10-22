87193810f609643648e42b415a0b3466b6883652
Natalia20041 <147888488+Natalia20041@users.noreply.github.com>
2025-10-22 16:59:13 -0500

    Fix matrix menu and improve user prompts

--- matriz-multiencadenada/src/Main.java
+++ matriz-multiencadenada/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         int opcion = 0;
+        int n = 0;
         NodoM raiz = null;
-        int ultimoN = 0;
 
         do {
             menu();
@@ -14,34 +14,42 @@ public class Main {
 
             if (opcion == 1) {
                 System.out.print("Ingrese tamaño de la matriz: ");
-                int n = keyboard.nextInt();
+                n = keyboard.nextInt();
                 System.out.println("Ingresa el valor minimo que tendra el valor de los nodos");
                 int minRandom = keyboard.nextInt();
                 System.out.println("Ingresa el valor maximo que tendra el valor de los nodos");
                 int maxRandom = keyboard.nextInt();
                 raiz = NodoM.crearMatriz(n, minRandom, maxRandom);
-                ultimoN = n;
-                System.out.println("Muy bien hecho, matriz creada");
+                System.out.println("Matriz creada correctamente:");
+                raiz.imprimirMatrizCuadrada(n);
+
             } else if (opcion == 2) {
-                if (raiz == null) {
-                    System.out.println("No hay matriz creada.");
-                } else {
-                    raiz.imprimirMatrizCuadrada(ultimoN);
-                }
+                if (raiz != null) raiz.imprimirMatrizCuadrada(n);
+                else System.out.println("Primero debe crear la matriz");
+
             } else if (opcion == 3) {
-                // implementar mostrar diagonal principal cuando lo necesites
+                if (raiz != null) raiz.mostrarDiagonalPrincipal(n);
+                else System.out.println("Primero debe crear la matriz");
+
             } else if (opcion == 4) {
-                // implementar mostrar diagonal secundaria cuando lo necesites
+                if (raiz != null) raiz.mostrarDiagonalSecundaria(n);
+                else System.out.println("Primero debe crear la matriz");
+
             } else if (opcion == 5) {
-                // implementar mostrar triangular inferior cuando lo necesites
+                if (raiz != null) raiz.mostrarTriangularInferior(n);
+                else System.out.println("Primero debe crear la matriz");
+
             } else if (opcion == 6) {
-                // implementar mostrar triangular superior cuando lo necesites
+                if (raiz != null) raiz.mostrarTriangularSuperior(n);
+                else System.out.println("Primero debe crear la matriz");
+
             } else if (opcion == 7) {
-                System.out.println(" Salir del programa");
+                System.out.println("Salir del programa");
                 System.out.println("Programa finalizado");
                 break;
+
             } else {
-                System.out.println(" Opcion invalida");
+                System.out.println("Opcion invalida");
             }
 
         } while (true);
@@ -52,11 +60,10 @@ public class Main {
         System.out.println("\n================== MENÚ MATRICES ==================");
         System.out.println("1 Crear matriz");
         System.out.println("2 Mostrar matriz");
-        System.out.println("3 Mostrear diagonal principal");
+        System.out.println("3 Mostrar diagonal principal");
         System.out.println("4 Mostrar diagonal secundaria");
         System.out.println("5 Mostrar triangular inferior");
         System.out.println("6 Mostrar triangular superior");
         System.out.println("7 Salir");
     }
 }
-
